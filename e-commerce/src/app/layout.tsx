0397eeee6d29a2c03cc964dfa4fcbe5f4e68b049
MattProznym <mateusz.prozny@imakeable.com>
2023-12-21 16:12:45 +0100

    Add basic packages, basic config and first demo component

---
+++ e-commerce/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+
+export const metadata = {
+  title: 'My Mantine app',
+  description: 'I have followed setup instructions carefully',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
