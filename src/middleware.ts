037d46b718c4af29cff4c2789c6871099c29e2f6
Bartosz Trusinski <bartosztrusinski@gmail.com>
2024-03-01 23:07:07 +0100

    simplify and cleanup middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,33 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
 import { i18n } from "@/i18n.config";
-
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  handleLocale(request);
+}
+
+function handleLocale(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const isLocaleInPathname = i18n.locales.some((locale) =>
+    pathname.startsWith(`/${locale}`)
+  );
+
+  if (isLocaleInPathname) {
+    return null;
+  }
+
+  const locale = getLocale(request);
+  const localeUrl = new URL(
+    `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
+    request.url
+  );
+
+  return locale === i18n.defaultLocale
+    ? NextResponse.rewrite(localeUrl)
+    : NextResponse.redirect(localeUrl);
+}
 
 function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {};
@@ -16,33 +39,6 @@ function getLocale(request: NextRequest): string | undefined {
 
   const locale = matchLocale(languages, locales, i18n.defaultLocale);
   return locale;
-}
-
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    if (locale === i18n.defaultLocale) {
-      return NextResponse.rewrite(
-        new URL(
-          `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-          request.url
-        )
-      );
-    }
-
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url
-      )
-    );
-  }
 }
 
 export const config = {
