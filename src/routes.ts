e2f6c1a0cbbb55c9a37474cc71707aecdba2042e
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-03-05 20:10:25 +0100

    adjust comments

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,22 @@
 /**
- * Routes that are accessible to the public
+ * Routes that are accessible to the public.
  * They do not require any authentication
  */
-export const publicRoutes: string[] = ["/", "/home"];
+export const publicRoutes: string[] = [
+  "/",
+  "/home",
+  "/auth/login",
+  "/auth/register",
+];
 
 /**
- * Routes that are used for authentication
+ * Routes that are used for authentication.
  * They are not accessible to logged in users and will redirect to default page
  */
 export const authRoutes: string[] = ["/auth/login", "/auth/register"];
 
 /**
- * Prefix for authentication API
+ * Prefix for authentication API.
  * Routes with this prefix should always be accessible for auth to work
  */
 export const apiAuthPrefix: string = "/api/auth";
@@ -19,4 +24,9 @@ export const apiAuthPrefix: string = "/api/auth";
 /**
  * The default redirect path after logging in
  */
-export const DEFAULT_LOGIN_REDIRECT: string = "/user";
+export const DEFAULT_REDIRECT_ON_LOGIN: string = "/user";
+
+/**
+ * The default redirect path after trying to access a protected route
+ */
+export const DEFAULT_REDIRECT_ON_UNAUTHORIZED: string = "/auth/login";
