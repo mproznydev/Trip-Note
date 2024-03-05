859faa8d30dc59479514b6f6ec34a1a0ad514d99
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-03-05 20:42:33 +0100

    add credentials login logic for action

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,7 +1,11 @@
 "use server";
 
-import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import * as z from "zod";
+
+import { signIn } from "@/auth";
+import { DEFAULT_REDIRECT_ON_LOGIN } from "@/routes";
+import { LoginSchema } from "@/schemas";
 
 export async function login(data: z.infer<typeof LoginSchema>) {
   const validatedFields = LoginSchema.safeParse(data);
@@ -10,5 +14,24 @@ export async function login(data: z.infer<typeof LoginSchema>) {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Logged in" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_REDIRECT_ON_LOGIN,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
+  }
 }
