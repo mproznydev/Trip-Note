455e07376c403fbe3d8c091f5201c41a84d5b5dd
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-03-05 20:29:34 +0100

    handle user authorization

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,9 +3,32 @@
   Do NOT merge this with the main auth file
 */
 
-import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcryptjs";
+import { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
+import { getUserByEmail } from "@/data/user";
+import { LoginSchema } from "@/schemas";
+
 export default {
-  providers: [Credentials],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (!validatedFields.success) return null;
+
+        const { email, password } = validatedFields.data;
+        const user = await getUserByEmail(email);
+
+        if (!user || !user.password) return null;
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordsMatch) return null;
+
+        return user;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
