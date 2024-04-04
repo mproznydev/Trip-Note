7a38ed6ca980fbe5076021cc15ec38b6c06c419f
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-04-04 23:33:30 +0200

    make validation stronger

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -10,12 +10,28 @@ export const LoginSchema = z.object({
 });
 
 export const RegisterSchema = z.object({
-  email: z.string().email({
-    message: "Please enter your email address",
-  }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters long",
-  }),
+  email: z
+    .string()
+    .min(1, {
+      message: "Please enter your email address",
+    })
+    .email({
+      message: "Please enter a valid email address",
+    }),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters long",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[0-9]/, {
+      message: "Password must contain at least one number",
+    }),
 });
 
 export const RegisterFormSchema = RegisterSchema.extend({
